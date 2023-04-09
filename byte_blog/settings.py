8609eb5a9448042b327fda6c3a56cb9571b16dd2
Akarsh711 <paliwalap7@gmail.com>
2023-04-10 02:43:59 +0530

    changing settings for aws

--- byte_blog/settings.py
+++ byte_blog/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
+# import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
@@ -142,4 +142,4 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 # AUTH_USER_MODEL = "blog.UserManage" 
 
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
